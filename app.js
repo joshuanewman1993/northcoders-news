fb0509f719701273e36bb73c4246d214f32eb7cb
nathanscott85 <nathansfeely@gmail.com>
2018-11-05 17:15:18 +0000

    changed a route

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-// const HomePage = require("./controllers/Homepage");
 const { DB_URL } =
   process.env.NODE_ENV === "production" ? process.env : require("./config");
 const { handle400, handle404, handle500 } = require("./error");
@@ -16,8 +15,6 @@ mongoose
   .connect(DB_URL)
   .then(() => console.log(`Connected to database ${DB_URL}`))
   .catch(console.log());
-
-// app.get("/", HomePage);
 
 // this is used to render the html page from your view folder
 app.get("/", (req, res, next) => {
