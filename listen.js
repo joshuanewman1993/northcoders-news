7d9c82e6dbf37ff6d2eaf74d6a82abf35a1ef095
nathanscott85 <nathansfeely@gmail.com>
2018-11-09 12:03:28 +0000

    resolved

--- listen.js
+++ listen.js
@@ -1,9 +1,9 @@
-// const { port } = process.env || 1802;
+const { port } = process.env || 1802;
 const app = require("./app");
 
-const port = 1802;
+// const port = 1802;
 
-app.listen($port, err => {
+app.listen(port, err => {
   if (err) throw err;
   console.log(`Server is listening on ${port}`);
 });
--- routes/topics-route.js
+++ routes/topics-route.js
@@ -1,18 +1,18 @@
-// const topicsRoute = require("express").Router();
-// console.log("before");
-// const {
-//   fetchAllTopics,
-//   fetchOneSlug,
-//   fetchAllArticlesBySlugId,
-//   postArticleBySlugId
-// } = require("../controllers/topics");
-// console.log("here");
-// topicsRoute.get("/", fetchAllTopics);
-// // fetch one topic by slug??!?
-// topicsRoute.route("/:slug").get(fetchOneSlug);
-// topicsRoute.route("/:slug/articles").get(fetchAllArticlesBySlugId);
-// topicsRoute.route("/:slug/articles").post(postArticleBySlugId);
+const topicsRoute = require("express").Router();
+console.log("before");
+const {
+  fetchAllTopics,
+  fetchOneSlug,
+  fetchAllArticlesBySlugId,
+  postArticleBySlugId
+} = require("../controllers/topics");
+console.log("here");
+topicsRoute.get("/", fetchAllTopics);
+// fetch one topic by slug??!?
+topicsRoute.route("/:slug").get(fetchOneSlug);
+topicsRoute.route("/:slug/articles").get(fetchAllArticlesBySlugId);
+topicsRoute.route("/:slug/articles").post(postArticleBySlugId);
 
-// // belongs_to is the slug
-// //on the req.params
-// module.exports = topicsRoute;
+// belongs_to is the slug
+//on the req.params
+module.exports = topicsRoute;
